ae4b6b829398c88b75c717234b2d99618d741df4
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-12 10:36:57 +0530

    callback error

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,12 +1,13 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
         vm.startBroadcast();
+        //Mock
         FundMe fumdMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
         return fumdMe;
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Idenfier:MIT
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+
+contract HelperConfig is Script {}
