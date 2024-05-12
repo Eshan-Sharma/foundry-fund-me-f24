177c2308a97a0d71ae6b76d02ff1463cf91eb01b
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-12 12:51:03 +0530

    prank-fake user, deal-fake money and makeAddress-fake address for testing

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,10 +9,15 @@ contract FundMeTest is Test {
     //First deploy the contract you want to test
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_VALUE = 10 ether;
+
     function setUp() external {
         //us -> FumdMeTest -> FundMe
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_VALUE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -31,5 +36,12 @@ contract FundMeTest is Test {
     function testFundFailsWithoutEnoughETH() public {
         vm.expectRevert();
         fundMe.fund();
+    }
+
+    function testFundUpdatesFundDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER); //Instead of finding which address sends in the message, we use pranking which is
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
