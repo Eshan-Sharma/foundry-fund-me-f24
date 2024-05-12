b0dca11857c89dc3c1fd04b63c0b4e3fec9f5569
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-12 10:26:32 +0530

    refactor to use deploy on any chain

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,14 +1,14 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
     // Using Oracle to get the price of ETH/USD
-    function getPrice() internal view returns (uint256) {
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         //Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
         (
             /* uint80 roundID */
             ,
@@ -22,8 +22,8 @@ library PriceConverter {
         return uint256(price) * 1e10;
     }
 
-    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
-        uint256 ethPrice = getPrice();
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
