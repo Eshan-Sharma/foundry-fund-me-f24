6bf33bb4decfde94731d493aad770b38b3da10bb
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-12 08:44:35 +0530

    copy from source

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,39 +1,41 @@
-//Get fund from users
-//Withdraw funds
-//Set a minimum funding value in USD
-
-//SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe__NotOwner();
+error NotOwner();
 
 contract FundMe {
-    address public immutable i_owner;
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
     constructor() {
         i_owner = msg.sender;
     }
 
-    using PriceConverter for uint256;
-    // Using constant - it is constant at compile time and no longer takes up storage spot
-
-    uint256 public constant MINIMUM_USD = 5 * 1e18; //Instead of minimum ETH, let's set minimum Usd value
-    address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
-
     function fund() public payable {
-        //1. How do we send ETH to this contract? - Use "payable"
-        // Allow users to send $
-        // Have a minimum $ sent
-
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); // 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-        // What is a revert?
-        // Undo any actions that have been done, and send the remaining gas back
-        // Even if the transaction reverts, since some computation occured. Gas is spent
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
     }
 
     function withdraw() public onlyOwner {
@@ -42,35 +44,43 @@ contract FundMe {
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        //withdraw the funds, 3 ways
-        //transfer
-        //msg.address, type of address
-        //payable(msg.address), type of payable address
+        // // transfer
         // payable(msg.sender).transfer(address(this).balance);
 
-        // //send
+        // // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
 
-        //call
+        // call
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
 
-    modifier onlyOwner() {
-        //this is executed first, kinda like middleware in js
-        // require(msg.sender == i_owner, "Sender is not owner!");//executed
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
-        _; //goes back to the function definition
-            //checks if there is anything else?
-            //nope, ends
+    fallback() external payable {
+        fund();
     }
 
     receive() external payable {
         fund();
     }
-
-    fallback() external payable {
-        fund();
-    }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
