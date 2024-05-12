f0ada8a1edf035ddf9a24bf968385428f09c883f
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-12 12:24:11 +0530

    refactoring

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,6 +8,9 @@ import {Script} from "lib/forge-std/src/Script.sol";
 import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed; //ETH/USD [price feed address]
     }
@@ -18,7 +21,7 @@ contract HelperConfig is Script {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -28,13 +31,14 @@ contract HelperConfig is Script {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) return activeNetworkConfig;
         //price feed address
         //1. deploy the mocks
         //2. get the address of the mocks
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
